92eaa1b33b734ccfee8a88be6030faa7a97d9393
Agustin Alonso <alonsoiagustin@gmail.com>
2025-03-22 14:27:05 +0100

    add additional environment variables

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,9 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: "./env/dev.env" });
 
-export const { HOST, PORT, URI } = process.env;
+export const { HOST, PORT, URI, APPNAME, CONTACT } = process.env;
 
-if (!HOST || !PORT || !URI) {
+if (!HOST || !PORT || !URI || !APPNAME || !CONTACT) {
   throw new Error(
     "Missing environment variable. Please check your .env file and ensure all required variables are defined."
   );
