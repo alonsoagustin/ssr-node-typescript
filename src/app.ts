4851155951eab002195ac9e25cf56f67409049f9
Agustin Alonso <alonsoiagustin@gmail.com>
2025-03-22 15:39:07 +0100

    add middleware for JSON and URL-encoded form parsing

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,9 @@ import { APPNAME, CONTACT } from "./config";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use(morgan("dev"));
 
 app.set("view engine", "ejs");
