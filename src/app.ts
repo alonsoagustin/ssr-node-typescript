2792f0f5845003400dfc1ace6237419089a055ef
Agustin Alonso <alonsoiagustin@gmail.com>
2025-03-22 14:28:33 +0100

    set up EJS view engine and configure authentication routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
+import path from "node:path";
 import express from "express";
 import morgan from "morgan";
+import authRouter from "./routes/authRoutes";
+import { APPNAME, CONTACT } from "./config";
 
 const app = express();
 
 app.use(morgan("dev"));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.locals.app = APPNAME;
+app.locals.contact = CONTACT;
+
+app.use("/auth", authRouter);
 
 export default app;
