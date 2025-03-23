14ab51d8834ee9875a4ed8ec87154a75de29d21f
Agustin Alonso <alonsoiagustin@gmail.com>
2025-03-23 21:27:42 +0100

    add postLogin middleware in authController

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import User from "../model/User";
 
 const renderLogin = (
   res: Response,
@@ -14,4 +15,31 @@ const renderLogin = (
 export const getLogin = (_req: Request, res: Response) => {
   res.status(200);
   renderLogin(res);
+};
+
+export const postLogin = async (req: Request, res: Response) => {
+  // Get email and password from the request body
+  const { email, password } = req.body;
+
+  // If email and password are not provided we render the login page with an error message
+  if (!email || !password) {
+    res.status(400);
+    renderLogin(res, email, "Please provide an email and password");
+    return;
+  }
+
+  // Try to find a user with the provided email
+  const user = await User.findOne({ email });
+
+  // if user does not exist or password is incorrect we render the login page with an error message
+  if (!user || !(await user.comparePassword(password))) {
+    res.status(401);
+    renderLogin(res, email, "Invalid email or password");
+    return;
+  }
+
+  // if user exists and password is correct we set the userId in the session and redirect to the products page
+  req.session.userId = user.id;
+
+  res.status(200).redirect("/products");
 };
