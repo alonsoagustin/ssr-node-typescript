d2b2ab0de589d216b694a204cf332631ca25977f
Agustin Alonso <alonsoiagustin@gmail.com>
2025-03-23 21:26:20 +0100

    add POST route for login in authRoutes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -4,5 +4,6 @@ import * as authController from "../controllers/authController";
 const router = Router();
 
 router.get("/login", authController.getLogin);
+router.post("/login", authController.postLogin);
 
 export default router;
