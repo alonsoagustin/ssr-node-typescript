7ce104f9b1e453272345b5622295281c4f62608c
Agustin Alonso <alonsoiagustin@gmail.com>
2025-03-24 21:25:10 +0100

    refactor productsController to render products page using path module

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -1,5 +1,7 @@
+import path from "node:path";
 import { Request, Response } from "express";
 
 export const getProducts = (_req: Request, res: Response) => {
-  res.send("ProductsPage");
+  res.locals.page = "../pages/products";
+  res.status(200).render(path.join(__dirname, "../views/layout/base.ejs"));
 };
