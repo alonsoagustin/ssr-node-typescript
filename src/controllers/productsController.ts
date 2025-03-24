7ee4fde26f8d6208fc268bd1b31a565f5d0fb86a
Agustin Alonso <alonsoiagustin@gmail.com>
2025-03-24 21:36:39 +0100

    add getNewProduct function to productsController for rendering new product page

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -5,3 +5,8 @@ export const getProducts = (_req: Request, res: Response) => {
   res.locals.page = "../pages/products";
   res.status(200).render(path.join(__dirname, "../views/layout/base.ejs"));
 };
+
+export const getNewProduct = (_req: Request, res: Response) => {
+  res.locals.page = "../pages/newProduct";
+  res.status(200).render(path.join(__dirname, "../views/layout/base.ejs"));
+};
