37bbf7e84c9fe6efee7e10dcb29f47391187c091
Agustin Alonso <alonsoiagustin@gmail.com>
2025-03-25 17:28:08 +0100

    add getProducts function to retrieve user-specific products and handle empty state

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -1,8 +1,24 @@
 import path from "node:path";
 import { Request, Response } from "express";
+import Product from "../model/Product";
 
-export const getProducts = (_req: Request, res: Response) => {
+export const getProducts = async (req: Request, res: Response) => {
+  // get all products of the user from the database
+  const products = await Product.find({ owner: req.session.userId });
+
+  // if there are no products, we render the products page with a error message
+  if (products.length < 1) {
+    res.locals.products = [];
+    res.locals.error = "You don't have any products yet.";
+    res.locals.page = "../pages/products";
+    res.status(200).render(path.join(__dirname, "../views/layout/base.ejs"));
+    return;
+  }
+
+  // if there are products, we render the products page with the products
+  res.locals.products = products;
   res.locals.page = "../pages/products";
+  res.locals.error = "";
   res.status(200).render(path.join(__dirname, "../views/layout/base.ejs"));
 };
 
