8407adc51a3da3964c6f5c2a8cec3ff7176c31d2
Agustin Alonso <alonsoiagustin@gmail.com>
2025-03-27 21:06:50 +0100

    add mongoStore to session manager for improved session handling

--- src/lib/sessionManager.ts
+++ src/lib/sessionManager.ts
@@ -1,6 +1,7 @@
 import session from "express-session";
+import mongoStore from "connect-mongo";
 import { Request, Response, NextFunction } from "express";
-import { APPNAME, CONTACT, SECRET_SESSION } from "../config";
+import { APPNAME, CONTACT, URI, SECRET_SESSION } from "../config";
 
 declare module "express-session" {
   interface SessionData {
@@ -13,6 +14,11 @@ export const createSession = session({
   secret: String(SECRET_SESSION), // Secret used to sign the session ID cookie
   resave: false, // Forces the session to be saved back to the session store
   saveUninitialized: true, // Forces a session that is "uninitialized" to be saved to the store
+  store: mongoStore.create({
+    mongoUrl: String(URI), // MongoDB connection string
+    collectionName: "sessions", // Name of the collection to store sessions in
+    ttl: 2 * 24 * 60 * 60, // Time to live in seconds (2 days)
+  }),
   cookie: {
     maxAge: 1000 * 60 * 60 * 24 * 2, // The cookie will expire in 2 days
   },
