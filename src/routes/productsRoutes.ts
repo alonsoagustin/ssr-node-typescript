23fc74ecea321982abbd5b82807a01d8d6143bb9
Agustin Alonso <alonsoiagustin@gmail.com>
2025-03-27 12:59:27 +0100

    add routes for creating and deleting products

--- src/routes/productsRoutes.ts
+++ src/routes/productsRoutes.ts
@@ -4,12 +4,15 @@ import * as productsController from "../controllers/productsController";
 const router = Router();
 
 router.get("/", productsController.getProducts);
+
 router.get("/new", productsController.getNewProduct);
+router.post("/new", productsController.postNewProduct);
 
 router.get("/update/:productId", productsController.getUpdateProduct);
 router.post("/update/:productId", productsController.postUpdateProduct);
 
 router.get("/:productId", productsController.getProductDetails);
-router.delete("/:productId", productsController.deleteProduct);
+
+router.get("/delete/:productId", productsController.deleteProduct);
 
 export default router;
