2e34abddffb889cf8f19b8773ea662b20f781d0b
Agustin Alonso <alonsoiagustin@gmail.com>
2025-03-27 10:39:20 +0100

    refactor getProducts method to improve error handling and code clarity

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -3,23 +3,31 @@ import { Request, Response } from "express";
 import Product from "../model/Product";
 
 export const getProducts = async (req: Request, res: Response) => {
-  // get all products of the user from the database
-  const products = await Product.find({ owner: req.session.userId });
+  try {
+    // get userId from the session
+    const userId = req.session.userId;
 
-  // if there are no products, we render the products page with a error message
-  if (products.length < 1) {
-    res.locals.products = [];
-    res.locals.error = "You don't have any products yet.";
+    // get all products of the user from the database
+    const products = await Product.find({ owner: userId });
+
+    // if there are no products, we render the products page with a error message
+    if (products.length < 1) {
+      res.locals.products = products;
+      res.locals.error = "You don't have any products yet.";
+      res.locals.page = "../pages/products";
+      res.render(path.join(__dirname, "../views/layout/base.ejs"));
+      return;
+    }
+
+    // if there are products, we render the products page with the products
+    res.locals.products = products;
     res.locals.page = "../pages/products";
-    res.status(200).render(path.join(__dirname, "../views/layout/base.ejs"));
-    return;
+    res.render(path.join(__dirname, "../views/layout/base.ejs"));
+  } catch (error) {
+    // if there is an error, we render the serverError page
+    res.locals.page = "../pages/serverError";
+    res.render(path.join(__dirname, "../views/layout/base.ejs"));
   }
-
-  // if there are products, we render the products page with the products
-  res.locals.products = products;
-  res.locals.page = "../pages/products";
-  res.locals.error = "";
-  res.status(200).render(path.join(__dirname, "../views/layout/base.ejs"));
 };
 
 export const getNewProduct = (_req: Request, res: Response) => {
